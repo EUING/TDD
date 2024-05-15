721f7435bb5376947b4f16a8dab40a226058b6f0
euing <coke1324@naver.com>
2024-05-15 20:40:51 +0900

    입력받은 작업 아이템 표시 기능 추가

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,10 +2,14 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+import re
 
 from lists.views import home_page
 
 class HomePageTest(TestCase):
+    def remove_csrf(self, origin):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', origin)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -14,6 +18,21 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string("home.html")
-        self.assertEqual(response.content.decode(), expected_html)
+
+        expected_html = self.remove_csrf(render_to_string("home.html", request=request))
+        response_decode = self.remove_csrf(response.content.decode())
+
+        self.assertEqual (response_decode, expected_html)
         
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST["item_text"] = "신규 작업 아이템"
+        
+        response = home_page(request)
+        
+        response_decode = self.remove_csrf(response.content.decode())
+        self.assertIn("신규 작업 아이템", response_decode)
+        expected_html = self.remove_csrf(render_to_string("home.html", {"new_item_text": "신규 작업 아이템"}, request=request))
+        
+        self.assertEqual(response_decode, expected_html)
